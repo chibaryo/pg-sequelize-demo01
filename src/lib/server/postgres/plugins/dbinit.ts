a96adaece77afc7b51859b1ef125854a9b8a94c7
Ryo <swintschiba@gmail.com>
2023-12-10 01:20:51 +0900

    2mm

--- src/lib/server/postgres/plugins/dbinit.ts
+++ src/lib/server/postgres/plugins/dbinit.ts
@@ -1,7 +1,8 @@
 import { Sequelize } from 'sequelize'
-import { PSQL_USER, PSQL_PASSWD, PSQL_DBHOST, PSQL_DBNAME } from '$env/static/private'
-
-export const dbConfig = new Sequelize(`postgres://${PSQL_USER}:${PSQL_PASSWD}@${PSQL_DBHOST}/${PSQL_DBNAME}`)
+//import { PSQL_USER, PSQL_PASSWD, PSQL_DBHOST, PSQL_DBNAME } from '$env/static/private'
+import { SUPABASE_PSQL_USER, SUPABASE_PSQL_PASSWD, SUPABASE_PSQL_DBHOST, SUPABASE_PSQL_DBNAME } from '$env/static/private'
+//export const dbConfig = new Sequelize(`postgres://${PSQL_USER}:${PSQL_PASSWD}@${PSQL_DBHOST}/${PSQL_DBNAME}`)
+export const dbConfig = new Sequelize(`postgres://${SUPABASE_PSQL_USER}:${SUPABASE_PSQL_PASSWD}@${SUPABASE_PSQL_DBHOST}/${SUPABASE_PSQL_DBNAME}`)
 
 export const dbInit = async () => {
   try {
