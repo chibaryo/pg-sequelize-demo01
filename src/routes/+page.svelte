3dd4c0bb187dfc0d50d32c83a63f34dfa5e0635b
Ryo <swintschiba@gmail.com>
2023-12-10 01:04:24 +0900

    2mm

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -56,8 +56,10 @@
 
   const closeModal = async () => {
     isModalOpen = false
-    currentRow.name = ''
-    currentRow.prefCapital = ''
+    console.log("currentRow: ", currentRow)
+    currentRow["uuid"] = ""
+    currentRow["name"] = ""
+    currentRow["prefCapital"] = ""
   }
 
   const closeDeleteModal = async () => {
@@ -93,7 +95,7 @@
   tabindex="-1"
   aria-hidden="true"
   bind:this={is_hidden}
-  class="hidden overflow-y-auto overflow-x-hidden fixed top-20 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+  class="hidden overflow-y-auto overflow-x-hidden fixed top-[12vh] left-0 right-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
     <div class="relative p-4 w-full max-w-md max-h-full">
         <!-- Modal content -->
         <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
