c4c05a400b63fb5e46cd6ecf017e987efe9eba2c
Ryo <swintschiba@gmail.com>
2023-12-10 09:23:09 +0900

    2mm

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -25,6 +25,14 @@
   let currentRow: PrefectureDoc
 
   // fn
+  const handleAddRow = async () => {
+    if (currentRow) {
+      currentRow["uuid"] = ""
+      currentRow["name"] = ""
+      currentRow["prefCapital"] = ""
+    }
+    isModalOpen = true
+  }
   const handleEditRow = async (row: PrefectureDoc) => {
     currentRow = row
     isModalOpen = true
@@ -52,6 +60,10 @@
 
     isDeleteModalOpen = false
 
+    currentRow["uuid"] = ""
+    currentRow["name"] = ""
+    currentRow["prefCapital"] = ""
+    console.log("currentRow: ", currentRow)
   }
 
   const closeModal = async () => {
@@ -85,7 +97,7 @@
   data-modal-target="authentication-modal"
   data-modal-toggle="authentication-modal"
   class="btn btn btn-sm btn-outline btn-success"
-  type="button" on:click={() => isModalOpen = true}>
+  type="button" on:click={() => handleAddRow()}>
   新規登録
 </button>
 
@@ -197,21 +209,26 @@
               <svg class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                   <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
               </svg>
-              <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to delete this product?</h3>
+              <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">このレコードを削除しますか？</h3>
+              {#if currentRow}
+              <div>
+                <span class="inline-block">{currentRow.name}</span>
+              </div>
+              {/if}
               <button
                 data-modal-hide="popup-modal"
                 type="button"
                 on:click={() => deleteRow(currentRow)}
                 class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center me-2"
                 >
-                  Yes, I'm sure
+                  削除
               </button>
               <button
                 data-modal-hide="popup-modal"
                 type="button"
                 class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
                 on:click={() => closeDeleteModal()}
-                >No, cancel</button>
+                >キャンセル</button>
           </div>
       </div>
   </div>
